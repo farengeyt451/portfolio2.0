8c270922fba90a84df2a07a998530846710b0cbe
Alexander Kislov <strannik11.92@gmail.com>
2019-05-01 00:39:02 +0300

    Fixes

--- src/app/components/portfolio/swiper.config.ts
+++ src/app/components/portfolio/swiper.config.ts
@@ -2,13 +2,13 @@ import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 export const swiperConfig: SwiperConfigInterface = {
   direction: 'horizontal',
-  // loop: true,
   resistanceRatio: 0,
   speed: 800,
   slidesPerView: 3,
   spaceBetween: 50,
   navigation: true,
   keyboard: true,
+  slideToClickedSlide: false,
   breakpoints: {
     1200: {
       slidesPerView: 3,
