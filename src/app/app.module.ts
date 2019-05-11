3b3a5310c0f01ed935c85797f4a9731a400f35e5
Alexander Kislov <strannik11.92@gmail.com>
2019-05-11 15:20:34 +0300

    Rebuild contacts page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { InlineSVGModule } from 'ng-inline-svg';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { ParticlesModule } from 'angular-particle';
@@ -26,7 +28,9 @@ import { ContactsComponent } from './components/contacts';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
+    InlineSVGModule.forRoot(),
     ParticlesModule,
     BrowserAnimationsModule,
     SwiperModule,
--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.component.sass']
 })
 export class ContactsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
