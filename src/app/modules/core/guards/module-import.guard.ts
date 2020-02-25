c8897fa82017064f5b9cb4688e6f035ea9cd536f
Alexander Kislov <strannik11.92@gmail.com>
2020-02-25 21:33:17 +0300

    Some enhances

--- src/app/modules/core/guards/module-import.guard.ts
+++ src/app/modules/core/guards/module-import.guard.ts
@@ -1,7 +1,6 @@
 export function throwIfAlreadyLoaded(parentModule: any, moduleName: string) {
-  if (parentModule) {
-    throw new Error(
-      `${moduleName} has already been loaded. Import ${moduleName} modules in the AppModule only.`
-    );
-  }
+  if (!parentModule) return;
+  throw new Error(
+    `${moduleName} has already been loaded. Import ${moduleName} modules in the AppModule only.`
+  );
 }
